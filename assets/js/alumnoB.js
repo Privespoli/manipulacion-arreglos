264265653a7ff3f40eb46592c8adc881e6793632
Melina <melina.dmec@gmail.com>
2026-03-23 10:42:37 +0100

    eliminar la ciudad p2

---
+++ assets/js/alumnoB.js
@@ -0,0 +1,9 @@
+// Melina - B
+console.log("Alumno B ");
+
+let rutasEspeciales = ["Bilbao", "Zaragoza", "Cádiz", "Barcelona", "Málaga"];
+
+// eliminar la ciudad en la posición 2
+rutasEspeciales.splice(2, 1);
+console.log("Después de eliminar la posición 2:", rutasEspeciales);
+
