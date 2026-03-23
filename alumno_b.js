06baa3d9a0397ea4222886f3bb68e7425c890888
Melina <melina.dmec@gmail.com>
2026-03-23 10:45:18 +0100

    invertir el orden rutas

--- alumno_b.js
+++ alumno_b.js
@@ -1,4 +1,4 @@
-// Melina - B
+// Melina B
 console.log("Alumno B ");
 
 let rutasEspeciales = ["Bilbao", "Zaragoza", "Cádiz", "Barcelona", "Málaga"];
@@ -6,4 +6,8 @@ let rutasEspeciales = ["Bilbao", "Zaragoza", "Cádiz", "Barcelona", "Málaga"];
 // eliminar la ciudad en la posición 2
 rutasEspeciales.splice(2, 1);
 console.log("Después de eliminar la posición 2:", rutasEspeciales);
+
+// invertir el orden de las rutas
+rutasEspeciales.reverse();
+console.log("Rutas al revés:", rutasEspeciales);
 
